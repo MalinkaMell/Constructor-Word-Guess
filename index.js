5bd5f9667dfa9e6e8f42a45f45f97a93081da27f
Maimell <malinka77@gmail.com>
2019-10-26 23:12:44 -0700

    added some validations

--- index.js
+++ index.js
@@ -2,34 +2,48 @@ let inquirer = require("inquirer");
 //let inputChar = process.argv[2];
 let Word = require("./word");
 
-let wordToGuess = new Word ("hellou"); // is this supposed to grab the word from an array of words?
+let wordToGuess = new Word("hellou"); // is this supposed to grab the word from an array of words?
 let count = 0;
-let userGuesses = 3;
+let userGuesses = 8;
 
 function showWord() {
     if (count < userGuesses) {
         inquirer
-        .prompt([
-            {
-                name: "letter",
-                type: "input",
-                message: "Guess a letter"
-            }
-        ])
-        .then(function (answers) {
-            let guessed = answers.letter;
-            wordToGuess.checkString(guessed);
-            wordToGuess.wordString();
-            
-            count++;
-            showWord();
-        })
-       
+            .prompt([
+                {
+                    name: "letter",
+                    type: "input",
+                    message: "Guess a letter"
+                }
+            ])
+            .then(function (answers) {
+                let guessed = answers.letter;
+                wordToGuess.checkString(guessed);
+                wordToGuess.wordString();
+
+                function checkWord(el) {
+                    return (el.guessed === true);
+                }
+
+                if (wordToGuess.arr.every(checkWord) === true) {
+                    console.log("congrats!");
+
+                }
+
+
+
+                count++;
+                showWord();
+
+            })
+
     } else {
-console.log("You are out of guesses");
+        console.log("You are out of guesses");
 
     }
-    
-   // console.log(wordToGuess);
+
+    // console.log(wordToGuess);
 }
+
+
 showWord();
